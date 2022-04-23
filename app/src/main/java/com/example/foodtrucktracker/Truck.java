37f25cb2ae82dbd69cf7a00b36ed9ab84341d3a2
Nghiem Truong <vinhnghiemcr@gmail.com>
2022-04-23 01:21:52 -0500

    Update TimelineFragment and TrucksAdapter

--- app/src/main/java/com/example/foodtrucktracker/Truck.java
+++ app/src/main/java/com/example/foodtrucktracker/Truck.java
@@ -1,13 +1,17 @@
 package com.example.foodtrucktracker;
 
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
+@ParseClassName("Truck")
 public class Truck extends ParseObject {
     public static final String KEY_TRUCK_NAME = "name";
     public static final String KEY_OPEN_HOURS= "open_hours";
     public static final String KEY_TRUCK_DESCRIPTION = "description";
     public static final String KEY_PHONE_NUMBER = "phone_number";
+    public static final String KEY_IMAGE = "image";
     public static final String KEY_USER = "user";
 
     public String getTruckName() {return getString(KEY_TRUCK_NAME);}
@@ -29,6 +33,14 @@ public class Truck extends ParseObject {
     public String getPhoneNumber() {return getString(KEY_PHONE_NUMBER);}
 
     public void setPhoneNumber(String phoneNumber) {put(KEY_PHONE_NUMBER, phoneNumber);}
+
+    public ParseFile getImage() {
+        return getParseFile(KEY_IMAGE);
+    }
+
+    public void setImage(ParseFile parseFile) {
+        put(KEY_IMAGE, parseFile);
+    }
 
     public ParseUser getUser() {
         return getParseUser(KEY_USER);
